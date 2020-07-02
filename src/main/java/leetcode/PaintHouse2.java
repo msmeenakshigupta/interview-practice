2a6980a967b5f8d35592201c22ba5a2763e1d438
msmeenakshigupta <66375478+msmeenakshigupta@users.noreply.github.com>
2020-07-02 17:22:56 +0530

    Create PaintHouse2.java
    There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
    
    The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
    
    Note:
    All costs are positive integers.
    
    Example:
    
    Input: [[1,5,3],[2,9,4]]
    Output: 5
    Explanation: Paint house 0 into color 0, paint house 1 into color 2. Minimum cost: 1 + 4 = 5; 
                 Or paint house 0 into color 2, paint house 1 into color 0. Minimum cost: 3 + 2 = 5. 
    Follow up:
    Could you solve it in O(nk) runtime?

---
+++ src/main/java/leetcode/PaintHouse2.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int minCostII(int[][] costs) {
+        if (costs.length == 0) return 0;
+        
+        int n = costs.length;
+        int k = costs[0].length;
+        
+        for (int house = 1; house < n; house++) {
+            for (int color = 0; color < k; color++) {
+                int min = Integer.MAX_VALUE;
+                for (int previousColor = 0; previousColor < k; previousColor++) {
+                    if (color == previousColor) continue;
+                    min = Math.min(min, costs[house - 1][previousColor]);
+                }
+                costs[house][color] += min;
+            }
+        }
+
+        // Find the minimum in the last row.
+        int min = Integer.MAX_VALUE;
+        for (int c : costs[n - 1]) {
+            min = Math.min(min, c);
+        }
+        return min;
+    }
+}
