162fe30acc6954ffb2247d8cd1916b816b4592f9
msmeenakshigupta <66375478+msmeenakshigupta@users.noreply.github.com>
2020-07-02 16:25:08 +0530

    Create MeetingRooms.java
    Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
    
    Example 1:
    
    Input: [[0,30],[5,10],[15,20]]
    Output: false
    Example 2:
    
    Input: [[7,10],[2,4]]
    Output: true

---
+++ src/main/java/leetcode/MeetingRooms.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+         Comparator<int[]> c=(int[] a, int[] b) -> (a[0]-b[0]);
+        Arrays.sort(intervals, c);
+        for(int i=0; i<intervals.length-1; i++)
+            if(intervals[i][1]>intervals[i+1][0]) return false;
+        return true;
+    }
+}
